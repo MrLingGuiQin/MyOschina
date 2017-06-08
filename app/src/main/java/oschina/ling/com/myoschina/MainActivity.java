2d85f38c4b901b4594c3730ad749be9a9003ce2e
ling <824801022@qq.com>
2017-06-09 00:10:11 +0800

    完成toolbar的设置

--- app/src/main/java/oschina/ling/com/myoschina/MainActivity.java
+++ app/src/main/java/oschina/ling/com/myoschina/MainActivity.java
@@ -1,20 +1,23 @@
 package oschina.ling.com.myoschina;
 
-import android.support.v7.app.ActionBar;
 import android.os.Bundle;
 import android.support.v4.app.FragmentTabHost;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TabHost;
 import android.widget.TextView;
 
 import butterknife.BindView;
-import butterknife.ButterKnife;
 import oschina.ling.com.myoschina.base.BaseActivity;
 
 public class MainActivity extends BaseActivity {
     @BindView(android.R.id.tabhost)
     FragmentTabHost mTabhost;
+    @BindView(R.id.tb_toolbar)
+    Toolbar mToolbar;
 
     @Override
     public int getLayoutResId() {
@@ -23,14 +26,21 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void initView(Bundle savedInstanceState) {
-        initActionBar();
+        initToolbar();
         initTab();
     }
 
-    private void initActionBar() {
-        ActionBar actionBar = getSupportActionBar();
-        actionBar.setTitle(getString(R.string.app_name));
-        actionBar.setLogo(getResources().getDrawable(R.drawable.ic_drawer));
+    private void initToolbar() {
+        setSupportActionBar(mToolbar);
+        mToolbar.setNavigationIcon(R.drawable.ic_drawer);
+        mToolbar.setTitle(getString(R.string.app_name));
+        mToolbar.setOnMenuItemClickListener(mOnMenuItemClickListener);
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showToast("666", 1);
+            }
+        });
     }
 
     private void initTab() {
@@ -54,10 +64,32 @@ public class MainActivity extends BaseActivity {
 
     }
 
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        // TODO: add setContentView(...) invocation
-        ButterKnife.bind(this);
+    public void onClick(View v) {
+        switch (v.getId()) {
+        }
     }
+
+
+    // 设置菜单按钮点击时间
+    private Toolbar.OnMenuItemClickListener mOnMenuItemClickListener = new Toolbar.OnMenuItemClickListener() {
+        @Override
+        public boolean onMenuItemClick(MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.menu_search:
+                    showToast("开始搜索啦啦啦---", 1);
+                    break;
+            }
+            return true;
+        }
+    };
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main,menu);
+        return true;
+    }
+
+
 }
---
+++ app/src/main/java/oschina/ling/com/myoschina/MyFragmentTabhost.java
@@ -0,0 +1,40 @@
+package oschina.ling.com.myoschina;
+
+import android.content.Context;
+import android.support.v4.app.FragmentTabHost;
+import android.util.AttributeSet;
+
+/**
+ * Created by Administrator on 2017/6/7.
+ * 定制FragmentTabhost
+ */
+
+public class MyFragmentTabhost extends FragmentTabHost {
+    private String mCurrentTabId;
+
+    private String mNoChangeTabId;
+
+
+    public MyFragmentTabhost(Context context) {
+        super(context);
+    }
+
+    public MyFragmentTabhost(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    // tab点击切换回调监听
+    @Override
+    public void onTabChanged(String tabId) {
+        if (tabId.equals(mNoChangeTabId)) {
+            setCurrentTabByTag(mCurrentTabId);
+        }
+        super.onTabChanged(tabId);
+        mCurrentTabId = tabId;
+    }
+
+    public void setNoChangeTabId(String noChangeTabId) {
+        mNoChangeTabId = noChangeTabId;
+    }
+
+}
